8b2311f41eb80c2ef4056a98ddd2a3c0c4aaab23
zadaz <ada@AdaMacBookPro.local>
2016-06-18 15:11:55 +0800

    随便同步一下

--- calculator.py
+++ calculator.py
@@ -1,7 +1,21 @@
+# -*- coding:utf-8 -*-
+
 import tornado.web
 import tornado.ioloop
+
+# 试图写一个计算器玩，木有可点击界面。你打我呀~~~
+#
 
 class Calculator(tornado.web.RequestHandler):
 	def get(self):
 		self.write('hello world')
 
+
+class Damu(tornado.web.RequestHandler):
+	def get(self):
+
+def main():
+
+
+if __name__=="__main__":
+	main()
--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
+# -*- coding:utf-8 -*-
+
 import tornado.web
 import tornado.ioloop
+
+#Web framework
+#tornado.web — RequestHandler and Application classes
+#tornado.web.RequestHandler 是一个类
+#tornado.web.Application 也是一个类
 
 class Damu(tornado.web.RequestHandler):
 	def get(self):
 		self.write('hello world')
+# get是这个handler的方法，get没有定义的。
+# write也是一个方法method，这个handle已经定义过了。将括号内的内容(object)以class定义的method(方式)pass(传递)过去。
 
 class SmallDamu(tornado.web.RequestHandler):
 	"""docstring for ClassName"""
@@ -22,6 +31,14 @@ def main():
 		(r"/s",SmallDamu)])
 	app.listen(1234)
 	tornado.ioloop.IOLoop.instance().start()
+# [list]，{dictionary}
+# 把class实例化成一个object
+# r是一个字符串的修饰符，意思是后面是个路径？？？待确认
+# 这段适合CV式coding，实际是一个ioloop包，里面再一个IOLoop包，把instance这个class实例化
+# instance（）不传参数，它就是一直没有特点的猫。
+
 
 if __name__=="__main__":
 	main()
+
+#这个也cv吧！调用主程序的惯用方法。
